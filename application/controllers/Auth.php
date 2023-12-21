52c82c0fed202be45a9b4e81495f63992b6d56a0
eri-maulana <maulanabouble11@gmail.com>
2023-12-21 14:34:29 +0700

    menambah halaman admin dan user

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -41,7 +41,11 @@ class Auth extends CI_Controller
                   'role_id' => $user['role_id']
                ];
                $this->session->set_userdata($data);
-               redirect('user');
+               if ($user['role_id'] == 1) {
+                  redirect('admin');
+               } else {
+                  redirect('user');
+               }
             } else {
                // if password is wrong 
                $this->session->set_flashdata('message', '<div class="alert alert-danger text-center" role="alert">Wrong Password.!</div>');
